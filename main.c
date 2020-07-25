ac12cdf095526d83a8727e48011b5bd666c9c2a0
mo <mo3789530@gmail.com>
2020-07-26 00:45:26 +0900

    add method

--- main.c
+++ main.c
@@ -3,45 +3,80 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef enum { 
+typedef enum
+{
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-
-typedef enum {
+typedef enum
+{
   PREPARE_SUCCESS,
-  PREPARE_UNRECIGNIZED_STATEMENT
+  PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
-typedef enum {
+typedef enum
+{
   STATEMENT_INSERT,
   STATEMENT_SELECT
 } StatementType;
 
-typedef struct {
+typedef struct
+{
   StatementType type;
 } Statement;
 
-
-typedef struct {
-  char*   buffer;
-  size_t  buffer_length;
-  ssize_t input_length;
+typedef struct
+{
+  char *buffer;
+  size_t buffer_length;
+  size_t input_length;
 } InputBuffer;
 
-
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
-  if(strcmp(input_buffer->buffer, ".exit") == 0) {
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0)
+  {
+    close_input_buffer(input_buffer);
     exit(EXIT_SUCCESS);
   }
-  else {
+  else
+  {
     return META_COMMAND_UNRECOGNIZED_COMMAND;
   }
 }
 
-InputBuffer* new_input_buffer() {
-  InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+  {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  it(strcmp(input_buffer->buffer, "select") == 0)
+  {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+  switch (statement->type)
+  {
+  case (STATEMENT_INSERT):
+    printf("This is where we would do an insert.\n");
+    break;
+  case (STATEMENT_SELECT):
+    printf("This is where we would do a select.\n");
+    break;
+  }
+}
+
+InputBuffer *new_input_buffer()
+{
+  InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
   input_buffer->input_length = 0;
@@ -50,29 +85,32 @@ InputBuffer* new_input_buffer() {
 
 void print_prompt() { printf("db > "); }
 
-void read_input(InputBuffer* input_buffer) {
-  ssize_t bytes_read = 
-    getline(&(input_buffer->buffer),
-        &(input_buffer->buffer_length),stdin);
-  if(bytes_read <= 0) {
+void read_input(InputBuffer *input_buffer)
+{
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer),
+              &(input_buffer->buffer_length), stdin);
+  if (bytes_read <= 0)
+  {
     printf("Error: input reade error\n");
     exit(EXIT_FAILURE);
   }
   input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read -1] = 0;
+  input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer* input_buffer) {
+void close_input_buffer(InputBuffer *input_buffer)
+{
   free(input_buffer->buffer);
   free(input_buffer);
 }
 
-int main(int argc, char* argv[]) {
-  InputBuffer* input_buffer = new_input_buffer();
-  while(true) {
+int main(int argc, char *argv[])
+{
+  InputBuffer *input_buffer = new_input_buffer();
+  while (true)
+  {
     print_prompt();
     read_input(input_buffer);
-
   }
 }
-
