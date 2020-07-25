4c707d8144c2dd8e6da769e8c3ef6195722e35a6
mo3789530 <mo3789530@gmail.com>
2020-07-26 01:18:55 +0900

    end of chapter2

--- main.c
+++ main.c
@@ -33,6 +33,12 @@ typedef struct
   size_t input_length;
 } InputBuffer;
 
+void close_input_buffer(InputBuffer *input_buffer)
+{
+  free(input_buffer->buffer);
+  free(input_buffer);
+}
+
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
   if (strcmp(input_buffer->buffer, ".exit") == 0)
@@ -53,7 +59,7 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     statement->type = STATEMENT_INSERT;
     return PREPARE_SUCCESS;
   }
-  it(strcmp(input_buffer->buffer, "select") == 0)
+  if(strcmp(input_buffer->buffer, "select") == 0)
   {
     statement->type = STATEMENT_SELECT;
     return PREPARE_SUCCESS;
@@ -99,12 +105,6 @@ void read_input(InputBuffer *input_buffer)
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void close_input_buffer(InputBuffer *input_buffer)
-{
-  free(input_buffer->buffer);
-  free(input_buffer);
-}
-
 int main(int argc, char *argv[])
 {
   InputBuffer *input_buffer = new_input_buffer();
@@ -112,5 +112,26 @@ int main(int argc, char *argv[])
   {
     print_prompt();
     read_input(input_buffer);
+    if(input_buffer->buffer[0] == '.') {
+      switch(do_meta_command(input_buffer)) {
+        case(META_COMMAND_SUCCESS):
+          continue;
+        case(META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecignized command '%s'", input_buffer->buffer );
+          continue;
+      }
+    }
+
+    Statement statement;
+    switch(prepare_statement(input_buffer, &statement)) {
+      case(PREPARE_SUCCESS):
+        break;
+      case(PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecoginzed keyword at start of '%s' \n",input_buffer->buffer );
+        continue;
+    }
+
+    execute_statement(&statement);
+    printf("Executed \n");
   }
 }
